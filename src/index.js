04985a039aa81ae8b0642c3d1e6fa861e19e87e3
Aleksandr Yefremenkov <yefremenkov.alek@gmail.com>
2022-04-01 19:43:11 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import {React} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Provider } from 'react-redux'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
-  );
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
